ad723bb11a4f38c78788142dfc8d64a9534482a5
Katja Süss <k.suess@rohberg.ch>
2020-12-25 20:40:57 +0100

    proxy removed

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
 const applyConfig = (config) => {
-  config.settings.searchkit = {
-    ...config.settings.searchkit,
-    esProxyWhitelist: {
-      GET: ['^/_aliases', '^/_all'],
-      POST: ['^/_search', /^\/[\w\d.-]+\/_search/],
-    },
+  config.settings.searchkitblock = {
+    ...config.settings.searchkitblock,
+    elasticurl: process.env.ELASTIC_URL || 'http://localhost:9200/plone2020',
   };
-
-  if (__SERVER__) {
-    // TODO: develop this further to support multiple proxied ES
-    const target = process.env.ELASTIC_URL || 'http://localhost:9200';
-    const esProxyMiddleware = require('./proxy').default;
-    config.settings.expressMiddleware = [
-      ...(config.settings.expressMiddleware || []),
-      esProxyMiddleware(target),
-    ];
-  }
-
   return config;
 };
 
