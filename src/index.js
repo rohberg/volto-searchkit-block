e5c263b5a117b1de3ef7146bc158b5606a80820f
Katja Süss <k.suess@rohberg.ch>
2020-12-25 18:44:07 +0100

    proxy

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 const applyConfig = (config) => {
+  config.settings.searchkit = {
+    ...config.settings.searchkit,
+    esProxyWhitelist: {
+      GET: ['^/_aliases', '^/_all'],
+      POST: ['^/_search', /^\/[\w\d.-]+\/_search/],
+    },
+  };
+
+  if (__SERVER__) {
+    // TODO: develop this further to support multiple proxied ES
+    const target = process.env.ELASTIC_URL || 'http://localhost:9200';
+    const esProxyMiddleware = require('./proxy').default;
+    config.settings.expressMiddleware = [
+      ...(config.settings.expressMiddleware || []),
+      esProxyMiddleware(target),
+    ];
+  }
+
   return config;
 };
 
