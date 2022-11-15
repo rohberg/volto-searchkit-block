443ae71c069fa254b3b60edcd1a8647a6a654cd5
Katja Süss <k.suess@rohberg.ch>
2022-11-15 17:21:25 +0100

    Create StateLogger.jsx

---
+++ src/components/StateLogger.jsx
@@ -0,0 +1,22 @@
+import {
+  withState,
+} from 'react-searchkit';
+
+class _StateLogger extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          Current query state <pre>{JSON.stringify(this.props.currentQueryState, null, 2)}</pre>
+        </div>
+        <div>
+          Current results state <pre>{JSON.stringify(this.props.currentResultsState, null, 2)}</pre>
+        </div>
+      </div>
+    );
+  }
+}
+
+const StateLogger = withState(_StateLogger);
+
+export default StateLogger;
