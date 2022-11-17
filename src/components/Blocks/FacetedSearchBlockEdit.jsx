5fc08e6044bab9a17809fdd55e21351b55d4feff
Katja Süss <k.suess@rohberg.ch>
2022-11-17 11:03:14 +0100

    Show searchblock in edit mode

--- src/components/Blocks/FacetedSearchBlockEdit.jsx
+++ src/components/Blocks/FacetedSearchBlockEdit.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import { SidebarPortal } from '@plone/volto/components';
 
 import Sidebar from './Sidebar';
-// import FacetedSearch from '../Views/FacetedSearch';
+import FacetedSearch from '../Views/FacetedSearch';
 
 const Edit = ({ data, onChangeBlock, block, selected }) => {
   return (
     <div className="block searchkitsearch">
       <SidebarPortal selected={selected}>
-        <Sidebar data={data} block={block} onChangeBlock={onChangeBlock} />
+        <Sidebar 
+          data={data}
+          block={block}
+          onChangeBlock={onChangeBlock} />
       </SidebarPortal>
 
-      <p><b>Searchkit search. Voll elastisch.</b></p>
+      <div className="block searchkitsearch">
+        <FacetedSearch data={data} />
+      </div>
     </div>
   );
 };
