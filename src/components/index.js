127717590fe91662b2927e99a844347924012430
Katja Süss <k.suess@rohberg.ch>
2023-03-02 20:32:10 +0100

    Fix linting warnings

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,8 @@ import FacetedSearchBlockEdit from './Blocks/FacetedSearchBlockEdit';
 import FacetedSearchBlockView from './Blocks/FacetedSearchBlockView';
 import TestSearchkitQuerystrings from './Views/TestSearchkitQuerystrings';
 
-export { FacetedSearchBlockEdit, FacetedSearchBlockView, TestSearchkitQuerystrings }
+export {
+  FacetedSearchBlockEdit,
+  FacetedSearchBlockView,
+  TestSearchkitQuerystrings,
+};
