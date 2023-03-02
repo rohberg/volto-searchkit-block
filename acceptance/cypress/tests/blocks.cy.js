4e78a7ffa78863c1f16e7c0d1ad87fcc098ac615
Katja Süss <k.suess@rohberg.ch>
2023-03-02 20:07:21 +0100

    First simple test

--- acceptance/cypress/tests/blocks.cy.js
+++ acceptance/cypress/tests/blocks.cy.js
@@ -33,57 +33,20 @@ context('Blocks Acceptance Tests', () => {
       cy.navigate('/document/edit');
     });
 
-    it('As editor I can add a Grid', function () {
+    it('As editor I can add a searchkit-clock', function () {
       cy.intercept('PATCH', '/**/document').as('edit');
       cy.intercept('GET', '/**/document').as('content');
       cy.intercept('GET', '/**/Document').as('schema');
 
       cy.getSlate().click();
       cy.get('.button .block-add-button').click({ force: true });
-      cy.get('.blocks-chooser .mostUsed .button.__grid').click({ force: true });
-      cy.findByText('2 columns').click();
+      cy.get('div[aria-label="Unfold Common blocks"]').click();
+      cy.get('.blocks-chooser .common .button.searchkitblock').click({
+        force: true,
+      });
 
-      cy.get('button[aria-label="Add grid block in position 0"]').click();
-      cy.get('.blocks-chooser .mostUsed .button.image').click();
-      cy.get('.block.image .toolbar-inner .buttons:first-child').click();
-      cy.get('[aria-label="Select My Image"]').dblclick();
-      cy.findByText('my-image');
-
-      cy.get('button[aria-label="Add grid block in position 1"]').click();
-      cy.get('.blocks-chooser [aria-label="Unfold Text blocks"]').click();
-      cy.get('.blocks-chooser .text .button.text').click();
-      cy.get('.block.inner.__grid .public-DraftEditor-content').type(
-        'Colorless green ideas sleep furiously.',
-      );
-
-      cy.get('#toolbar-save').click();
-      cy.wait('@edit');
-      cy.wait('@content');
-
-      cy.findByText('Colorless green ideas sleep furiously.');
-
-      cy.navigate('/document/edit');
-      cy.wait('@schema');
-      cy.get('.block.inner.__grid').click();
-      cy.get('.block.inner.__grid [aria-label="Reset grid element 1"]').click();
-      cy.get(
-        '.block.inner.__grid [aria-label="Remove grid element 1"]',
-      ).click();
-      cy.get(
-        '.block.inner.__grid .toolbar [aria-label="Add grid element"]',
-      ).click();
-      cy.get('button[aria-label="Add grid block in position 1"]').click();
-      cy.get('.blocks-chooser .mostUsed .button.teaser').click();
-      cy.get(
-        '.objectbrowser-field[aria-labelledby="fieldset-default-field-label-href"] button[aria-label="Open object browser"]',
-      ).click();
-      cy.get('[aria-label="Select My Page"]').dblclick();
-      cy.get('#toolbar-save').click();
-      cy.wait('@edit');
-      cy.wait('@content');
-
-      cy.get('.block.__grid').findByText('My Page');
+      // TODO Continue tests
+      // See volto grid block tests or similar
     });
-
   });
 });
