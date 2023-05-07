b422b5b4940aed3db062b7dba600035923f59e7d
Katja Süss <k.suess@rohberg.ch>
2023-05-07 18:53:23 +0200

    Bookmarks: Add export button for filter mapping to block sidebar configuration.

--- src/components/Blocks/FacetedSearchBlockEdit.jsx
+++ src/components/Blocks/FacetedSearchBlockEdit.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Container, Segment } from 'semantic-ui-react'
 import { SidebarPortal } from '@plone/volto/components';
 
 import Sidebar from './Sidebar';
 import FacetedSearch from '../Views/FacetedSearch';
+import DownloadFiltersMapping from './DownloadFiltersMapping';
 
 const Edit = ({ data, onChangeBlock, block, selected }) => {
   return (
     <div className="block searchkitsearch">
       <SidebarPortal selected={selected}>
         <Sidebar data={data} block={block} onChangeBlock={onChangeBlock} />
+        <Container>
+          <Segment textAlign="center">
+            <DownloadFiltersMapping data={data} />
+          </Segment>
+        </Container>
       </SidebarPortal>
 
       <div className="block searchkitsearch">
