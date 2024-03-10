2ec093533fc51256363678a2af79795b227cf652
Katja Süss <k.suess@rohberg.ch>
2024-03-10 10:27:13 +0100

    Update basic.cy.js

--- acceptance/cypress/tests/basic.cy.js
+++ acceptance/cypress/tests/basic.cy.js
@@ -39,33 +39,5 @@ context('Basic Acceptance Tests', () => {
       cy.wait('@content');
       cy.url().should('eq', Cypress.config().baseUrl + '/my-page');
     });
-
-    it('As editor I can add a link to a text block', function () {
-      cy.navigate('/document/edit');
-      cy.wait('@schema');
-
-      cy.get('.block.inner.title .documentFirstHeading');
-
-      cy.log('when I create a link');
-
-      cy.getSlateEditorAndType(
-        'Colorless green ideas sleep furiously.',
-      ).setSlateSelection('furiously');
-      cy.clickSlateButton('Add link');
-      cy.get('.slate-toolbar .link-form-container input').type(
-        'https://google.com{enter}',
-      );
-
-      cy.get('#toolbar-save', { timeout: 10000 }).click();
-      cy.wait('@content');
-
-      cy.log('then the page view should contain a link');
-
-      cy.get('.container p').contains('Colorless green ideas sleep furiously.');
-      cy.get('.container p a')
-        .should('have.text', 'furiously')
-        .and('have.attr', 'href')
-        .and('include', 'https://google.com');
-    });
   });
 });
