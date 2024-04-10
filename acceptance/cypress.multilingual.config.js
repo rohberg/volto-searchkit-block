40bfd01d21edae530255748723ef3f82d2b52945
Katja Süss <k.suess@rohberg.ch>
2024-04-10 19:40:46 +0200

    Fix typo

--- acceptance/cypress.multilingual.config.js
+++ acceptance/cypress.multilingual.config.js
@@ -3,7 +3,10 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   viewportWidth: 1280,
   e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
     baseUrl: 'http://localhost:3000',
-    specPattern: 'cypress/tests/*.mulltilingual.cy.{js,jsx}',
+    specPattern: 'cypress/tests/*.multilingual.cy.{js,jsx}',
   },
 });
