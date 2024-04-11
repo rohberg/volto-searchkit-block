61fd962a6bcd2903d6e31b0fd77020039eaf24de
Katja Süss <k.suess@rohberg.ch>
2024-04-11 09:05:48 +0200

    Loosen cypress default specPattern

--- acceptance/cypress.config.js
+++ acceptance/cypress.config.js
@@ -4,6 +4,6 @@ module.exports = defineConfig({
   viewportWidth: 1280,
   e2e: {
     baseUrl: 'http://localhost:3000',
-    specPattern: 'cypress/tests/*.singlelingual.cy.{js,jsx}',
+    specPattern: 'cypress/tests/*.cy.{js,jsx}',
   },
 });
