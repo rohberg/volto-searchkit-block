dd5eb22e4c072ae74927093e3cfcd6f944ab608b
Katja Süss <k.suess@rohberg.ch>
2024-06-08 07:54:48 +0200

    Update .project.eslintrc.js

--- .project.eslintrc.js
+++ .project.eslintrc.js
@@ -19,7 +19,8 @@ if (configFile) {
     voltoPath = `./${jsConfig.baseUrl}/${pathsConfig['@plone/volto'][0]}`;
 }
 
-const AddonConfigurationRegistry = require(`${voltoPath}/addon-registry.js`);
+// const AddonConfigurationRegistry = require(`${voltoPath}/addon-registry.js`);
+const AddonConfigurationRegistry = require('@plone/registry/src/addon-registry');
 const reg = new AddonConfigurationRegistry(projectRootPath);
 
 // Extends ESlint configuration for adding the aliases to `src` directories in Volto addons
@@ -35,6 +36,7 @@ module.exports = {
       alias: {
         map: [
           ['@plone/volto', '@plone/volto/src'],
+          ['@plone/volto-slate', '@plone/volto-slate/src'],
           ...addonAliases,
           ['@package', `${__dirname}/src`],
           ['@root', `${__dirname}/src`],
