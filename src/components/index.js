5a5276ab361229f889479eb192aaee5b9e143fa5
Katja Süss <k.suess@rohberg.ch>
2024-06-24 10:37:08 +0200

    Remove src/

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import FacetedSearchBlockEdit from './Blocks/FacetedSearchBlockEdit';
-import FacetedSearchBlockView from './Blocks/FacetedSearchBlockView';
-import TestSearchkitQuerystrings from './Views/TestSearchkitQuerystrings';
-
-export {
-  FacetedSearchBlockEdit,
-  FacetedSearchBlockView,
-  TestSearchkitQuerystrings,
-};
