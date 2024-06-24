efb1a028acd01cbadb3e4e4d6d205e1b324b0522
Katja Süss <k.suess@rohberg.ch>
2024-06-24 10:28:35 +0200

    Delete variables.mk

--- variables.mk
+++
@@ -1,7 +0,0 @@
-PLONE_VERSION=6.0.11.1
-VOLTO_VERSION=17.16.0
-
-ADDON_NAME='@rohberg/volto-searchkit-block'
-ADDON_PATH='volto-searchkit-block'
-KGS=plone.restapi==9.4.2 plone.volto==4.3.0 plone.rest==4.1.3
-TESTING_ADDONS=plone.app.robotframework==2.1.2 plone.app.testing==7.0.2
