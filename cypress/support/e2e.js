3b0c46ab9969cd6f55adb832d8a930f12f5aa9b7
Katja Süss <k.suess@rohberg.ch>
2024-10-28 21:55:40 +0100

    Update e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -4,6 +4,21 @@ import './commands';
 import 'cypress-axe';
 import { setup, teardown } from '@plone/volto/cypress/support/reset-fixture';
 
+// // 1. Disable Cypress uncaught exception failures from React hydration errors
+// Cypress.on('uncaught:exception', (err) => {
+//   // Cypress and React Hydrating the document don't get along
+//   // for some unknown reason. Hopefully, we figure out why eventually
+//   // so we can remove this.
+//   if (
+//     /ResizeObserver loop/.test(err.message) ||
+//     /hydrat/i.test(err.message) ||
+//     /Minified React error #418/.test(err.message) ||
+//     /Minified React error #423/.test(err.message)
+//   ) {
+//     return false;
+//   }
+// });
+
 beforeEach(function () {
   cy.log('Setting up API fixture');
   setup();
