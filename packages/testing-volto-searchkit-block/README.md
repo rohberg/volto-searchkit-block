1bd651bbcc112c050b145473dfbe22838eb14dcc
Katja Süss <k.suess@rohberg.ch>
2024-10-31 09:53:15 +0100

    Add testing fixtures package

---
+++ packages/testing-volto-searchkit-block/README.md
@@ -0,0 +1,5 @@
+# testing-volto-searchkit-block (testing-volto-searchkit-block)
+
+fixtures for testing
+
+- multilingual / monolingual
---
+++ packages/testing-volto-searchkit-block/src/index.js
@@ -0,0 +1,21 @@
+export const multilingualFixture = (config) => {
+  config.settings.isMultilingual = true;
+  config.settings.supportedLanguages = ['de', 'en'];
+  config.settings.defaultLanguage = 'de';
+
+  return config;
+};
+
+export const monolingualFixture = (config) => {
+  config.settings.isMultilingual = false;
+  config.settings.supportedLanguages = ['de'];
+  config.settings.defaultLanguage = 'de';
+
+  return config;
+};
+
+const applyConfig = (config) => {
+  return config;
+};
+
+export default applyConfig;
