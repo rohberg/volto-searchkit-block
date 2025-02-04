c5573b4c8ef725bf343a623e745c63b8d73bdb87
Katja Süss <k.suess@rohberg.ch>
2025-02-04 11:53:11 +0100

    Update CustomResultsListItemWithBookmarks

--- packages/testing-volto-searchkit-block/src/components/CustomResultsListItemWithBookmarks.jsx
+++ packages/testing-volto-searchkit-block/src/components/CustomResultsListItemWithBookmarks.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import truncate from 'lodash/truncate';
 import { Item } from 'semantic-ui-react';
-import { FormattedDate } from '@plone/volto/components';
+import FormattedDate from '@plone/volto/components/theme/FormattedDate/FormattedDate';
 import { flattenESUrlToPath } from '@rohberg/volto-searchkit-block/components/helpers';
 import cx from 'classnames';
 import { ToggleBookmarkButton } from '@plone-collective/volto-bookmarks/components';
+// import UniversalLink from '@plone/volto/components/manage/UniversalLink/UniversalLink';
 
 const CustomResultsListItemWithBookmarks = (props) => {
   const { result } = props;
@@ -15,25 +17,33 @@ const CustomResultsListItemWithBookmarks = (props) => {
     (state) => state.query?.data.showNewsItemPublishedDate,
   );
   return (
-    <Item
-      key={`item_${result['@id']}`}
-      className={cx('searchkitresultitem', result.review_state)}
-    >
-      <Item.Content>
-        <Item.Header as={Link} to={item_url}>
-          {result.title}
-        </Item.Header>
-        <Item.Meta>
-          {showNewsItemPublishedDate?.includes(result.portal_type) &&
-          result.effective ? (
-            <FormattedDate date={result.effective} locale={locale} />
-          ) : null}
-        </Item.Meta>
-        <Item.Meta>
-          <ToggleBookmarkButton item={result} />
-        </Item.Meta>
-      </Item.Content>
-    </Item>
+    <React.Fragment>
+      <Item
+        key={`item_${result['@id']}`}
+        className={cx('searchkitresultitem', result.review_state)}
+      >
+        <Item.Content>
+          <Item.Header as={Link} to={item_url}>
+            {result.title}
+          </Item.Header>
+          <Item.Meta>
+            {showNewsItemPublishedDate?.includes(result.portal_type) &&
+            result.effective ? (
+              <FormattedDate date={result.effective} locale={locale} />
+            ) : null}
+          </Item.Meta>
+
+          <Item.Description>
+            {truncate(result.description, { length: 200 })}
+          </Item.Description>
+          <Item.Extra>
+            <div className="right floated">
+              <ToggleBookmarkButton item={result} />
+            </div>
+          </Item.Extra>
+        </Item.Content>
+      </Item>
+    </React.Fragment>
   );
 };
 
