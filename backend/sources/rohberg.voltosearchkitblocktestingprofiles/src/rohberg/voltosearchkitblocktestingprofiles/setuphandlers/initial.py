f90fde2ee192ad6dca867e88fc28b5577c200470
Katja Süss <k.suess@rohberg.ch>
2025-03-05 16:22:19 +0100

    Add service clear-index for dev purpose

--- backend/sources/rohberg.voltosearchkitblocktestingprofiles/src/rohberg/voltosearchkitblocktestingprofiles/setuphandlers/initial.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-from plone import api
-from plone.exportimport import importers
-from Products.GenericSetup.tool import SetupTool
-from rohberg.voltosearchkitblocktestingprofiles import logger
